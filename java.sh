220fcc093e74e044a69bcc04c4cb7755e67a59c4
oliver <1749927373@qq.com>
2021-12-27 10:50:05 +0800

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,19 +1,19 @@
+#!/bin/bash
 #######################
 # write by zch        #
 # Java Generic Script #
 # version:20211225    #
 #######################
-#!/bin/bash
 NAME="" #项目名称
-WD="$(cd `dirname $0`/..; pwd)" #项目目录
+WD="$(cd "$(dirname "$0")"/.. || exit 1; pwd)" #项目目录
 MEM="" #JVM最大内存,单位MB
 NewSize="" #JVM新生代内存,单位MB
-JAR="gaia.jar" #项目jar包名字
+JAR="" #项目jar包名字
 CONF_DIR="${WD}/config" #application.yml位置
 LOGS_DIR="${WD}/logs" #项目启动后的日志输出位置
 PID_FILE="${WD}/bin/pid" #项目进程号
 NEED_DING_MESSAGE="no" #是否需要钉钉消息通知, "yes" or "no"
-WEBHOOK='' #钉钉通知群机器人的webhook地址
+WEBHOOK="" #钉钉通知群机器人的webhook地址
 MESSAGE="" #钉钉通知消息内容
 AT_ALL="true" #是否@所有人, "true" or "false"
 #输出绿色字符
@@ -29,15 +29,15 @@ showRed()
 }
 #stop project
 java_stop() {
-  MYPID=$(cat ${PID_FILE})
-  ps -ef | grep ${MYPID} | grep -v grep >> /dev/null
-  if [ $? -eq 0 ];then
+  MYPID=$(cat "${PID_FILE}")
+  if ps -ef | grep "${MYPID}" | grep -v grep >> /dev/null;
+  then
     showGreen "stoping ${NAME}, please wait a moment..."
-    kill ${MYPID}
+    kill "${MYPID}"
     sleep 10
-    ps -ef | grep ${MYPID} | grep -v grep >> /dev/null
-    if [ $? -eq 0 ];then
-      kill -9 ${MYPID}
+    if ps -ef | grep "${MYPID}" | grep -v grep >> /dev/null;
+    then
+      kill -9 "${MYPID}"
       showRed "${NAME} stop failed, but was forcibly stopped by kill -9."
     else
       showGreen "${NAME} stoped successfully"
@@ -49,17 +49,17 @@ java_stop() {
 #start project
 java_start() {
   BUILD_ID=dontKillMe
-  nohup java -jar -Xms${MEM}M -Xmx${MEM}M -XX:NewSize=${NewSize}M -Xmn${NewSize}M -XX:SurvivorRatio=8 -XX:MetaspaceSize=512M -XX:MaxMetaspaceSize=512M -Dspring.config.location=${CONF_DIR}/application.yml -XX:+UseG1GC -XX:+PrintGCDetails -Xloggc:${LOGS_DIR}/gc.log -Duser.timezone=GMT+8 ${WD}/${JAR} >> ${LOGS_DIR}/catalina.out 2>&1 &
-  echo $! > ${WD}/bin/pid
-  showGreen "${NAME} is starting, pid is $(cat ${PID_FILE})"
+  nohup java -jar -Xms"${MEM}"M -Xmx"${MEM}"M -XX:NewSize="${NewSize}"M -Xmn"${NewSize}"M -XX:SurvivorRatio=8 -XX:MetaspaceSize=512M -XX:MaxMetaspaceSize=512M -Dspring.config.location="${CONF_DIR}"/application.yml -XX:+UseG1GC -XX:+PrintGCDetails -Xloggc:"${LOGS_DIR}"/gc.log -Duser.timezone=GMT+8 "${WD}"/"${JAR}" >> "${LOGS_DIR}"/catalina.out 2>&1 &
+  echo $! > "${WD}"/bin/pid
+  showGreen "${NAME} is starting, pid is $(cat "${PID_FILE}")"
 }
 #start status check
 java_start_status_check() {
   showGreen "It is checking the status and will take one minute."
   for i in {1..11}
   do
-    netstat -ntlp | grep $(cat ${PID_FILE}) >> /dev/null
-    if [ $? -eq 0 ];then
+    if netstat -ntlp | grep "$(cat "${PID_FILE}")" >> /dev/null;
+    then
       STATUS="1"
       showGreen "${NAME} started successfully."
       break
@@ -69,15 +69,15 @@ java_start_status_check() {
     fi
   done
 
-  if [ ${STATUS} -eq 0 ];then
+  if [ "${STATUS}" -eq 0 ];then
     MESSAGE="${NAME} release failed, please call oliver to check."
     showRed "${MESSAGE}"
   fi
 }
 #dingding inform
 dingding_inform() {
-  if [ ${NEED_DING_MESSAGE} = yes ];then
-    curl -w '\n' -s ${WEBHOOK} \
+  if [ "${NEED_DING_MESSAGE}" = yes ];then
+    curl -w '\n' -s "${WEBHOOK}" \
       -H 'Content-Type: application/json' \
       -d '{
              "msgtype": "text",
@@ -89,18 +89,17 @@ dingding_inform() {
              }
           }'
   else
-    showRed "skip dingding inform, because variable "NEED_DING_MESSAGE" is no"
+    showRed "skip dingding inform, because variable NEED_DING_MESSAGE is no"
   fi
 }
 #view logs
 view_logs() {
-  tail -f ${LOGS_DIR}/catalina.out
+  tail -f "${LOGS_DIR}"/catalina.out
 }
 #project status
 check_pid() {
-  MYPID=$(cat ${PID_FILE})
-  ps -ef | grep ${MYPID} | grep -v grep >> /dev/null
-  if [ $? -eq 0 ];then
+  if ps -ef | grep "$(cat "${PID_FILE}")" | grep -v grep >> /dev/null;
+  then
     showGreen "${NAME} is runnning..."
   else
     showRed "${NAME} is not running, please check."
